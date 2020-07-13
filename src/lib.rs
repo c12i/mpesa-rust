622c6e3c542afcd63d751c1853c5187f271981f3
Collins Muriuki <murerwacollins@gmail.com>
2020-07-13 14:39:25 +0300

    (docs) WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 //! # mpesa
-//! A Rust wrapper around the MPESA REST API
+//! WIP
+//! ## About
+//! A Rust wrapper around the [Safaricom API](https://developer.safaricom.co.ke/docs?shell#introduction) for accessing M-Pesa services.
+//!
+//! This will allow you to make hassle free requests to the daraja API to perform B2B, B2C, and C2B transactions.
+//!
+//! Currently work in progress, documentation will be added every step of the way.
 
 #[cfg(test)]
 mod tests {
