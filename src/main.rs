3c94bca06c8daef75182581811331cdb68d51c53
Collins Muriuki <murerwacollins@gmail.com>
2020-07-14 16:15:02 +0300

    (dev) Can authenticate to daraja via snippet

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 #![allow(unused)]
 extern crate reqwest;
+
+use dotenv;
 use std::collections::HashMap;
+use std::env;
+
 
 use reqwest::blocking::{Client, self};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let resp = blocking::get("https://httpbin.org/ip")?
-    //     .json::<HashMap<String, String>>()?;
-    // println!("{:#?}", resp);
-    // Ok(())
+    // safaricom credentials
+    dotenv::dotenv().ok();
+    let username = env::var("CLIENT_KEY").unwrap();
+    let password = env::var("CLIENT_SECRET").unwrap();
 
     let client = Client::new();
+    let resp: HashMap<String,String> = client.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials")
+        .basic_auth(username, Some(password))
+        .send()?
+        .json()?;
+    println!("{:#?}", resp);
+
+    let t =resp.get("access_token").unwrap();
+
+    println!("token ==> {:?}", t);
+
     let res = client.post("http://httpbin.org/post")
         .body("the exact body that is sent")
-        .send()
-        .unwrap();
+        .send()?;
 
     println!("{:#?}", res);
     Ok(())
