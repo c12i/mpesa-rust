6ef3a4c2d2bb089a8dfb57d93caa5ae984e44da4
Collins Muriuki <murerwacollins@gmail.com>
2020-07-14 23:03:31 +0300

    (dev) Add extract_auth_token util function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,6 @@
 #![allow(unused)]
+use std::collections::HashMap;
+use std::error::Error;
 
 #[macro_export]
 macro_rules! map(
@@ -12,6 +14,11 @@ macro_rules! map(
        }
     };
 );
+
+pub fn extract_auth_token(hm: &HashMap<String, String>) -> Result<String, Box<dyn Error>> {
+    let token = hm.get("access_token").unwrap();
+    Ok(token.to_string())
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,5 +37,15 @@ mod tests {
         test_map.insert("two", 2);
 
         assert_eq!(macro_map, test_map);
+    }
+
+    #[test]
+    fn test_extract_auth_token() {
+       let map = map!(
+           String::from("access_token") => String::from("12345abcde"),
+           String::from("expires") => String::from("infinite")
+       );
+
+       assert_eq!(String::from("12345abcde"), extract_auth_token(&map).unwrap());
     }
 }
