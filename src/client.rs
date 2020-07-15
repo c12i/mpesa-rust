1556ad4316cb54499c8a2674fca8e21aff3429cf
Collins Muriuki <murerwacollins@gmail.com>
2020-07-15 10:07:42 +0300

    (dev) Refactor modules

---
+++ src/client.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+use std::error::Error;
+use reqwest::blocking::Client;
+
+use super::utils::extract_auth_token;
+#[derive(Debug)]
+pub enum Environment {
+    Production,
+    Sandbox,
+}
+#[derive(Debug)]
+pub struct Mpesa {
+    client_key: String,
+    client_secret: String,
+    base_url: String 
+}
+
+impl Mpesa {
+    pub fn new(client_key: String, client_secret: String, environemt: Environment) -> Mpesa {
+        let base_url = match environemt {
+            Environment::Production => String::from("https://api.safaricom.co.ke"),
+            Environment::Sandbox => String::from("https://sandbox.safaricom.co.ke"),
+        };
+
+        Mpesa {
+            client_key,
+            client_secret,
+            base_url,
+        }
+    }
+
+    pub fn auth(&self) -> Result<String, Box<dyn Error>> {
+        let url = format!("{}/oauth/v1/generate?grant_type=client_credentials", &self.base_url);
+
+        let resp: HashMap<String, String> = Client::new().get(&url)
+            .basic_auth(&self.client_key, Some(&self.client_secret))
+            .send()?
+            .json()?;
+        
+        Ok(extract_auth_token(&resp)?)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,56 +7,7 @@
 //!
 //! Currently work in progress, documentation will be added every step of the way.
 
-use std::collections::HashMap;
-use std::error::Error;
-use reqwest::blocking::{Client};
+pub mod utils;
+mod client;
 
-mod utils;
-
-pub use utils::extract_auth_token;
-
-pub enum Environment {
-    Production,
-    Sandbox,
-}
-
-pub struct Mpesa {
-    client_key: String,
-    client_secret: String,
-    base_url: String 
-}
-
-impl Mpesa {
-    pub fn new(client_key: String, client_secret: String, environemt: Environment) -> Mpesa {
-        let base_url = match environemt {
-            Environment::Production => String::from("https://api.safaricom.co.ke"),
-            Environment::Sandbox => String::from("https://sandbox.safaricom.co.ke"),
-        };
-
-        Mpesa {
-            client_key,
-            client_secret,
-            base_url,
-        }
-    }
-
-    pub fn auth(&self) -> Result<String, Box<dyn Error>> {
-        let url = format!("{}/oauth/v1/generate?grant_type=client_credentials", &self.base_url);
-
-        let resp: HashMap<String, String> = Client::new().get(&url)
-            .basic_auth(&self.client_key, Some(&self.client_secret))
-            .send()?
-            .json()?;
-        
-        Ok(extract_auth_token(&resp)?)
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub use client::{Mpesa,Environment};
