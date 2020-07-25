ae863b769a945b7d02d621b1e964728beea98c62
Collins Muriuki <murerwacollins@gmail.com>
2020-07-25 14:19:42 +0300

    (feat) complete b2b and b2b response structs

--- src/payloads/b2b.rs
+++ src/payloads/b2b.rs
@@ -2,7 +2,24 @@ use crate::CommandId;
 
 #[derive(Debug)]
 pub struct B2bPayload<'a> {
-    initiator_name: &'a str,
-    security_credentials: &'a str,
-    command_id: CommandId,
+    pub initiator_name: &'a str,
+    pub security_credentials: &'a str,
+    pub command_id: CommandId,
+    pub amount: u32,
+    pub party_a: &'a str,
+    pub sender_id: &'a str,
+    pub party_b: &'a str,
+    pub receiver_id: &'a str,
+    pub remarks: &'a str,
+    pub queue_timeout_url: &'a str,
+    pub result_url: &'a str,
+    pub occasion: &'a str,
 }
+
+#[derive(Debug)]
+pub struct B2bResponse<'a> {
+    conversation_id: &'a str,
+    originator_conversation_id: &'a str,
+    response_description: &'a str,
+}
+
