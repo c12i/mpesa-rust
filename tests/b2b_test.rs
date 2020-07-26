bd20c8bbca6f29197a6573882655e82cc4c4b967
Collins Muriuki <murerwacollins@gmail.com>
2020-07-27 00:49:21 +0300

    Add integration: ignoring b2c due to error 500

---
+++ tests/b2b_test.rs
@@ -0,0 +1,32 @@
+use mpesa::{Mpesa,Environment};
+use dotenv;
+use std::env;
+
+#[test]
+fn b2b_test() {
+    dotenv::dotenv().ok();
+
+    let client = Mpesa::new(
+        env::var("CLIENT_KEY").unwrap(),
+        env::var("CLIENT_SECRET").unwrap(),
+        Environment::Sandbox, // or environment variable
+        env::var("INIT_PASSWORD").unwrap(),
+    );
+
+    let b2b_response = client.b2b(
+        "testapi496",
+        1000,
+        "600496",
+        4,
+        "600000",
+        4,
+        "gg",
+        "https://muriuki.dev",
+        "https://muriuki.dev/blog",
+        "254708374149",
+    ).unwrap();
+
+    println!("B2b response -> {:#?}", b2b_response);
+
+    assert_eq!(b2b_response.ResponseCode, "0".to_string());
+}
