0597ec09d2c731e080895ae617fc20cf47803c8b
Collins Muriuki <murerwacollins@gmail.com>
2020-08-22 12:47:17 +0300

    Add account_balance payload

---
+++ src/payloads/account_balance.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+use crate::{CommandId,IdentifierTypes};
+
+#[derive(Debug)]
+pub struct AccountBalancePayload<'a> {
+    pub initiator_name: &'a str,
+    pub security_credentials: &'a str,
+    pub command_id: CommandId,
+    pub party_a: &'a str,
+    pub identifier_type: IdentifierTypes,
+    pub remarks: &'a str,
+    pub queue_timeout_url: &'a str,
+    pub result_url: &'a str,
+}
+
+/// B2C response
+/// Field names deliberately in Pascal case to correctly deserialize the
+/// response data
+#[derive(Debug,Deserialize)]
+pub struct AccountBalanceResponse {
+    pub OriginatorConverstionID: String,
+    pub ConversationID: String,
+    pub ResponseDescription: String,
+}
