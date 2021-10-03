1b1f8b3716a655aa4acd5b5335d2859d3066fb01
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-10-04 02:12:10 +0300

    Remove redundant syn import

--- mpesa_derive/src/lib.rs
+++ mpesa_derive/src/lib.rs
@@ -2,7 +2,6 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
 
 #[proc_macro_derive(MpesaSecurity)]
 pub fn mpesa_security_derive(input: TokenStream) -> TokenStream {
