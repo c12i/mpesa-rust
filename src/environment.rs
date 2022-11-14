e8f5f546268cc1bbdac2ddd5c992efcd6e514d42
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-11-14 18:26:35 +0300

    Make base_url only public in the context of the crate; remove redundant doc test

--- src/environment.rs
+++ src/environment.rs
@@ -52,16 +52,7 @@ impl TryFrom<&'static str> for Environment {
 
 impl Environment {
     /// Matches to intended base_url depending on Environment variant
-    ///
-    /// ## Example
-    /// ```
-    /// use mpesa::Environment;
-    ///
-    /// let env: Environment = Environment::Production;
-    /// let base_url: &str = env.base_url();
-    /// assert_eq!("https://api.safaricom.co.ke", base_url);
-    /// ```
-    pub fn base_url(&self) -> &'static str {
+    pub(crate) fn base_url(&self) -> &'static str {
         match self {
             Environment::Production => "https://api.safaricom.co.ke",
             Environment::Sandbox => "https://sandbox.safaricom.co.ke",
