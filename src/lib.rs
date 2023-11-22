be22c0bbf686f47bab434997270d5131df8d785c
Yasir <goodwonder5@gmail.com>
2023-11-22 16:43:06 +0300

    chore(client): Generic `send` implementation (#89)
    * chore: add generic send method
    
    * chore: fix URL link
    
    * Fix error handling in API calls
    
    * Remove unused URL variable in B2cBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -14,4 +14,4 @@ pub use constants::{
 };
 pub use environment::ApiEnvironment;
 pub use environment::Environment::{self, Production, Sandbox};
-pub use errors::{ApiError, MpesaError, MpesaResult};
+pub use errors::{MpesaError, MpesaResult, ResponseError};
